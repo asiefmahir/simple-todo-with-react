bc2f17e25e55197fb35a18edaa0f90145931e226
Asief Mahir <asiefmahir@gmail.com>
2022-01-29 22:50:04 +0600

    Implemented the Create and Delete operation. Todo: Edit Operation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+
+
+  /**
+   * Todo = "todo 1"
+   * todo = {
+   *    id: Date.now(),
+   *    titile: "todo 1"
+   * }
+   */
+
+  const [todoTitle, setTodoTitle] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  /**
+   * todolist =- []
+   * "Todo 1" // setTodoList(["Todo 1"]) // todoList = ["Todo 1"]
+   * "Todo 2" // setTodoList(["Todo 1", "Todo 2"]) // todoList = ["Todo 1", "Todo 2"]
+   */
+
+  // setCount(count + 1) // 1 // 2 // count = 1 // count = 2
+
+
+  /***
+   * todoTitle = ""
+   * user tyuped = 'T' / setTodoTitle('T') // todoTitle = 'T'
+   * setTodoTitle(e.target.value)
+   */
+  const changeTodoTitle = (val) => { // val === e.target.value
+    setTodoTitle(val)
+    /**
+     * ""
+     * setTodoTitle("T") // todoTitle = "T"
+     */
+  };
+
+  const addTodo = (title) => {
+    if(!title) {
+      alert("Please provide a valid todo title")
+    } else {
+      const newTodo = {
+        id: Date.now(),
+        title: title
+      }
+      setTodoList([newTodo, ...todoList])
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input onChange = {(e) => changeTodoTitle(e.target.value)} value = {todoTitle} placeholder = 'Please Enter a Todo title' type = "text" name = "todo"/>
+        <button onClick = {(e) => {
+          e.preventDefault();
+          addTodo(todoTitle);
+          setTodoTitle("")
+        }} >
+          Add Todo
+        </button>
+      </form>
+      <ul>
+        {todoList.map((todo) => (
+          <li>
+            <span>{todo.title}</span>
+            <button>Edit</button>
+            <button onClick = {() => {
+              const newTodoList = todoList.filter((item) => item.id !== todo.id);
+              setTodoList(newTodoList)
+            }} >Delete</button>
+          </li>
+        ))}
+        {/* <li>
+          <span> Todo 1 </span>
+          <button>Edit</button>
+          <button>Delete</button>
+        </li>
+        <li>
+          <span> Todo 2 </span>
+          <button>Edit</button>
+          <button>Delete</button>
+        </li>
+        <li>
+          <span> Todo 3 </span>
+          <button>Edit</button>
+          <button>Delete</button>
+        </li>
+        <li>
+          <span> Todo 4 </span>
+          <button>Edit</button>
+          <button>Delete</button>
+        </li> */}
+      </ul>
     </div>
   );
 }
